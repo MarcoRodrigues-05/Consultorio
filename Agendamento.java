11e6392835133a455147bed5bb6eaf2b8ec08d64
MarcoRodrigues-05 <marcopaulosrodrigues04@gmail.com>
2024-10-22 20:08:14 -0300

    Agendamento.java

---
+++ Agendamento.java
@@ -0,0 +1,12 @@
+
+class Agendamento {
+
+    private int Agendamento;
+    private String data;
+    private String horario;
+    private int idConsultorio;
+    private int idMedico;
+    private int idPaciente;
+    //getters e setters
+
+}
